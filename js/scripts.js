b41b618087b574e22026fcd3ab7fb921458f98b9
rilsta <rstarnes9@gmail.com>
2016-03-09 10:26:26 -0800

    add styling to users page

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,15 @@ $(document).ready(function(){
   $(".fa-bars").click(function(){
     $(".main-menu").slideToggle();
   });
+
+  $(".fa-chevron-down").click(function(){
+    $(".main-form").slideToggle();
+    $(".fa-chevron-down").hide();
+    $(".fa-chevron-up").show();
+  });
+  $(".fa-chevron-up").click(function(){
+    $(".main-form").slideToggle();
+    $(".fa-chevron-down").show();
+    $(".fa-chevron-up").hide();
+  });
 });
